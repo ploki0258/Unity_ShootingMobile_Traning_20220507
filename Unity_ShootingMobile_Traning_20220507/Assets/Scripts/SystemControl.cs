dad4a7277a407152f93e0c8bfd7116743b9e1dd2
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2022-05-28 13:05:31 +0800

    更新跑步動畫

--- Unity_ShootingMobile_Traning_20220507/Assets/Scripts/SystemControl.cs
+++ Unity_ShootingMobile_Traning_20220507/Assets/Scripts/SystemControl.cs
@@ -15,6 +15,12 @@ namespace JACK
         private Joystick joystick;
         [SerializeField, Header("移動速度"), Range(0, 300)]
         private float speed = 3.5f;
+        [SerializeField, Header("角色方向圖示")]
+        private Transform traDirectionIcon;
+        [SerializeField, Header("角色方向圖示範圍")]
+        private float rangeDirectionIcon = 2.5f;
+        [SerializeField, Header("角色旋轉速度"), Range(0, 100)]
+        private float speedTurn = 1.5f;
         [SerializeField, Header("動畫參數跑步")]
         private string parameterWalk = "開關跑步";
 
@@ -31,6 +37,8 @@ namespace JACK
         private void Update()
         {
             //GetJoystickValue();
+            UpdateDirectionIconPos();
+            LookDirectionIcon();
             UpdateAnimation();
         }
 
@@ -57,13 +65,40 @@ namespace JACK
         }
 
         /// <summary>
+        /// 更新角色方向圖示的座標
+        /// </summary>
+        private void UpdateDirectionIconPos()
+        {
+            //新座標 = 角色座標 + 三維向量(虛擬搖桿的水平與垂直) * 方向圖示範圍
+            Vector3 pos = transform.position + new Vector3(-joystick.Vertical, 0.5f, joystick.Horizontal) * rangeDirectionIcon;
+
+            //更新方向圖示的座標 = 新座標
+            traDirectionIcon.position = pos;
+        }
+
+        /// <summary>
+        /// 面向方向圖示
+        /// </summary>
+        private void LookDirectionIcon()
+        {
+            //取得面向角度 = 四位元.面向角度(方向圖示 - 角色) - 方向圖示與角色的向量
+            Quaternion look = Quaternion.LookRotation(traDirectionIcon.position - transform.position);
+
+            //角色的角度 = 四位元.插植(角色的角度，面向角度，旋轉速度 * 一幀的時間)
+            transform.rotation = Quaternion.Lerp(transform.rotation, look, speedTurn * Time.deltaTime);
+
+            //角色的歐拉角度 = 三維向量(0，原本的歐拉角度，0)
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+
+        /// <summary>
         /// 更新動畫
         /// </summary>
         private void UpdateAnimation()
         {
             //是否跑步 = 虛擬搖桿 水平 不為0 或 垂直 不為0
             bool run = joystick.Horizontal != 0 || joystick.Vertical != 0;
-            ani.SetBool(parameterWalk,run);
+            ani.SetBool(parameterWalk, run);
         }
     }
 }
